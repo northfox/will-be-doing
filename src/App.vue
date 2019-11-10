ba2b22851269b64e70b3e558c007c3c27352ffe1
northfox <vulpes.engineer@gmail.com>
2019-11-10 12:31:49 +0900

    Add filtering select box

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="navbar-nav">
         <router-link class="navbar-brand" to="/">Will-Be-Doing</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/all"> 全部</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/touch"> 触りたいもの</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/taste">食べたいもの</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/hearing">聴きたいもの</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/eyesight">見たいもの</router-link>
-        <router-link class="nav-link" exact-active-class="active" to="/todos/smell">嗅ぎたいもの</router-link>
       </div>
     </nav>
-    <router-view class="p-3" />
+    <router-view class="pt-3" />
   </div>
 </template>
 
