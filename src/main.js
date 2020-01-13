226b94c901d9e3f31dbad49cbb95edc9a42e8615
northfox <vulpes.engineer@gmail.com>
2020-01-13 15:23:59 +0900

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import dayjs from 'dayjs'
+import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +10,7 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.prototype.$dayjs = dayjs
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
