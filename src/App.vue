4a46bda3d536f3443278e9615dd2bf68ca764ce3
northfox <vulpes.engineer@gmail.com>
2020-01-19 11:56:01 +0900

    Add validate button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark justify-content-between">
       <router-link class="navbar-brand" to="/">Will-Be-Doing</router-link>
+      <button class="btn btn-light" onclick="location.reload(true)">動作検証</button>
     </nav>
     <router-view class="pt-2" />
   </div>
